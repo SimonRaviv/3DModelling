4fa2e364bfc52d617ff17b84628234f713a65943
Simon <lionsimonlion@gmail.com>
2016-05-21 15:07:15 +0300

    Main changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,174 @@
-//
-//#include "3DModellingH.h"
-//#include "FileProcessingH.h"
-//#include "ICPAlgorithm.h"
-//
-//class PointCloudSet
-//{
-//public:
-//	vector<PointCloudT> set;
-//	vector<PointCloudT>::iterator it;
-//
-//	PointCloudSet();
-//
-//	~PointCloudSet();
-//
-//private:
-//
-//};
-//
-//PointCloudSet::PointCloudSet()
-//{
-//	this->it = set.begin();
-//}
-//PointCloudSet::~PointCloudSet()
-//{
-//}
-//
-//class SimpleOpenNIViewer
-//{
-//public:
-//	pcl::visualization::CloudViewer viewer;
-//	pcl::Grabber* interface;
-//	FileProcessing file;
-//	ICPAlgorithm icp_Alg;
-//	PointCloudSet point_cloud_list;
-//	bool pcd_flag;
-//	bool ply_flag;
-//	bool flag;
-//	int counter;
-//
-//	SimpleOpenNIViewer() : viewer("PCL OpenNI Viewer")
-//	{
-//		this->interface = new pcl::io::OpenNI2Grabber();
-//		this->pcd_flag = true;
-//		this->ply_flag = true;
-//		this->flag = true;
-//		this->counter = 0;
-//	}
-//	~SimpleOpenNIViewer()
-//	{
-//
-//	}
-//	void cloud_cb_(PointCloudConstPtr &cloud)
-//	{
-//
-//		//PointCloudPtr cloud_out(new PointCloudT);
-//		//std::vector<int> mapping;
-//		//PointCloudPtr cloud_in(new PointCloudT);
-//		//copyPointCloud(*cloud, *cloud_in);
-//		this->point_cloud_list.it = this->point_cloud_list.set.insert(this->point_cloud_list.it, *cloud);
-//		//pcl::io::savePLYFile("file" + std::to_string(frameNumber++) + ".ply", *cloud_out);
-//		if (!viewer.wasStopped())
-//			viewer.showCloud(cloud);
-//	}
-//
-//	void run()
-//	{
-//		boost::function<void(PointCloudConstPtr&)> f =
-//			boost::bind(&SimpleOpenNIViewer::cloud_cb_, this, _1);
-//
-//		this->interface->registerCallback(f);
-//
-//		this->interface->start();
-//
-//		while (!viewer.wasStopped())
-//			boost::this_thread::sleep(boost::posix_time::seconds(1));
-//
-//		this->interface->stop();
-//	}
-//
-//};
-//
-//int main()
-//{
-//	cout << "*** Video capturing started ***" << endl;
-//	SimpleOpenNIViewer v;
-//	v.run();
-//	FileProcessing fp;
-//	vector<PointCloudT>::size_type j;
-//	vector<int> mapping;
-//	cout << "*** Video capturing stopped ***" << endl;
-//	//PCLPointCloud2 pcl2;
-//	//bool format = true;
-//	//bool use_camera = true;
-//
-//	//v.file.makePLYFromPointCloudSet("PointCloud", pcl2, format, use_camera, &v.point_cloud_list.set);
-//
-//	//for (j = 0; j != v.point_cloud_list.set.size(); j++)
-//	//pcl::io::savePLYFile("plyFiles\\file" + std::to_string(j) + ".ply", v.point_cloud_list.set[j]);
-//	cout << "*** Filtering started ***" << endl;
-//	for (j = 0; j != v.point_cloud_list.set.size(); j++)
-//		removeNaNFromPointCloud(v.point_cloud_list.set[j], v.point_cloud_list.set[j], mapping);
-//	cout << "*** Filtering stopped ***" << endl;
-//
-//
-//	//pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
-//	//pcl::PointCloud<PointT>::Ptr cloud2(new pcl::PointCloud<PointT>);
-//	//loadPLYFile("plyFiles\\file29.ply", *cloud);
-//	//loadPLYFile("plyFiles\\file30.ply", *cloud2);
-//
-//	//PointCloudPtr p(new PointCloudT);
-//	ICPAlgorithm icp_Alg;
-//	//Matrix4f total_transformation;
-//	//PointCloudPtr merge(new PointCloudT);
-//	//*merge += *cloud;
-//	//*merge += *cloud2;
-//	//fp.save_point_cloud("before.ply", *merge);
-//	//cout << "Start!!!" << endl;
-//	//icp_Alg.aligning_two_pointcloud(*cloud, *cloud2, *p, 50, 0.1, total_transformation);
-//	//cout << "finished!!!" << endl;
-//	//cout << total_transformation << endl;
-//
-//	icp_Alg.register_with_result(v.point_cloud_list.set, 20, 0.05);
-//	//icp_Alg.build_3d_map(v.point_cloud_list.set, 30, 0.09);
-//	//icp_Alg.register_with_previous_aligned(v.point_cloud_list.set, 30, 0.1);
-//	return 0;
-//}
+#define NOMINMAX
+#include <windows.h>
+#include <windowsx.h>
+#include "3DModellingH.h"
+#include "FileProcessingH.h"
+#include "ICPAlgorithm.h"
+#include "PointCloudSet.h"
+#include "camera.h"
+
+
+class SimpleOpenNIViewer
+{
+	static SimpleOpenNIViewer *s_instance;
+
+	SimpleOpenNIViewer() : viewer("PCL OpenNI Viewer")
+	{
+		this->record_flag = -1;
+	}
+
+public:
+	visualization::CloudViewer viewer;
+	Camera camera;
+	PointCloudSet point_cloud_list;
+	int record_flag;
+
+	static SimpleOpenNIViewer *instance()
+	{
+		if (!s_instance)
+			s_instance = new SimpleOpenNIViewer;
+		return s_instance;
+	}
+
+	void cloud_cb_(PointCloudConstPtr &cloud)
+	{
+		if (record_flag == 1)
+		{
+			this->point_cloud_list.it = this->point_cloud_list.set.insert(this->point_cloud_list.it, *cloud);
+		}
+
+		if (!viewer.wasStopped())
+			viewer.showCloud(cloud);
+	}
+
+	void run()
+	{
+		boost::function<void(PointCloudConstPtr&)> f =
+			boost::bind(&SimpleOpenNIViewer::cloud_cb_, this, _1);
+
+		camera.registerCallback(f);
+
+		camera.start();
+
+		while (!viewer.wasStopped())
+			boost::this_thread::sleep(boost::posix_time::seconds(1));
+
+		camera.stop();
+	}
+
+};
+
+
+SimpleOpenNIViewer *SimpleOpenNIViewer::s_instance = 0;
+HWND hwnd, recordButton, stopRecordButton, build3dmodelbutton;
+//HINSTANCE hInstance;
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	MSG  msg;
+	WNDCLASS wc = { sizeof(wc) };
+	wc.lpszClassName = TEXT("Static Control");
+	wc.hInstance = hInstance;
+	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+	wc.lpfnWndProc = WndProc; // error: undefined reference to `WndProc(HWND__*, unsigned int, unsigned int, long)@16'
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	RegisterClass(&wc);
+	hwnd = CreateWindow(wc.lpszClassName, TEXT("Build 3D model using kinect "), WS_SYSMENU | WS_MINIMIZEBOX,
+		100, 100, 700, 270, 0, 0, hInstance, 0);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (msg)
+	{
+	case WM_CREATE:
+	{
+		recordButton = CreateWindow("button", "Record",
+			WS_VISIBLE | WS_CHILD | WS_BORDER,
+			30, 160, 120, 40,
+			hwnd, (HMENU)1, NULL, NULL);
+
+		stopRecordButton = CreateWindow("button", "Stop Record",
+			WS_VISIBLE | WS_CHILD | WS_BORDER,
+			180, 160, 120, 40,
+			hwnd, (HMENU)2, NULL, NULL);
+		EnableWindow(stopRecordButton, false);
+		build3dmodelbutton = CreateWindow("button", "build the sciene",
+			WS_VISIBLE | WS_CHILD | WS_BORDER,
+			330, 160, 120, 40,
+			hwnd, (HMENU)3, NULL, NULL);
+		EnableWindow(build3dmodelbutton, false);
+		break;
+	}
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case 1:
+		{
+
+			FileProcessing fp;
+			vector<PointCloudT>::size_type j;
+			vector<int> mapping;
+			cout << "*** Video capturing started ***" << endl;
+
+			SimpleOpenNIViewer::instance()->record_flag = 1;
+			SimpleOpenNIViewer::instance()->run();
+			cout << "*** Video capturing stopped ***" << endl;
+			cout << "*** Filtering started ***" << endl;
+			for (j = 0; j != SimpleOpenNIViewer::instance()->point_cloud_list.set.size(); j++)
+				removeNaNFromPointCloud(SimpleOpenNIViewer::instance()->point_cloud_list.set[j], SimpleOpenNIViewer::instance()->point_cloud_list.set[j], mapping);
+			cout << "*** Filtering stopped ***" << endl;
+			EnableWindow(stopRecordButton, true);
+			EnableWindow(recordButton, false);
+			EnableWindow(build3dmodelbutton, false);
+			break;
+		}
+		case 2:
+		{
+			SimpleOpenNIViewer::instance()->record_flag = 0;
+			SimpleOpenNIViewer::instance()->camera.stop();
+			EnableWindow(build3dmodelbutton, true);
+			EnableWindow(recordButton, true);
+			EnableWindow(stopRecordButton, false);
+			break;
+		}
+		case 3:
+		{
+
+			ICPAlgorithm icp_Alg;
+			icp_Alg.register_with_result(SimpleOpenNIViewer::instance()->point_cloud_list.set, 20, 0.05);
+			EnableWindow(build3dmodelbutton, false);
+			EnableWindow(recordButton, false);
+			EnableWindow(stopRecordButton, false);
+			break;
+		}
+		default:
+			break;
+		}
+	}
+
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
